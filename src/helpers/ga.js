3d14d31dbb024822fc278d7fcfcc0c4d3e6f5115
g00dv1n <g00dv1n.private@gmail.com>
2020-02-02 18:24:04 +0200

    fix GA

--- src/helpers/ga.js
+++ src/helpers/ga.js
@@ -1,10 +1,16 @@
-const gaInstance = 'ga' in window && ga.getAll()[0];
+function getGaInstance() {
+    return 'ga' in window && ga.getAll()[0];
+}
 
 function fireSubscription() {
+    const gaInstance = getGaInstance();
+
     gaInstance && gaInstance.send('event', 'subscription', 'subscribe');
 }
 
 function firePin() {
+    const gaInstance = getGaInstance();
+
     gaInstance && gaInstance.send('event', 'pin', 'try-to-pin-after-subscription');
 }
 
